3cc7e61d71e58704e4402a7cf9c9d0104566d341
SamiChafik <sami.chafik00@gmail.com>
2025-05-04 22:04:26 +0100

    page to show all recipes + pop-up for details

---
+++ src/app/components/recipe-list/recipe-list.component.ts
@@ -0,0 +1,54 @@
+import { Component } from '@angular/core';
+import { RecipeService } from '../../services/recipe.service';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-recipe-list',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './recipe-list.component.html',
+  styleUrl: './recipe-list.component.scss'
+})
+export class RecipeListComponent {
+
+  recipes: any[] = [];
+  defaultImage = 'assets/logo_color.png';
+  selectedRecipe: any = null;
+
+  constructor(
+    private recipeService: RecipeService,
+    private router: Router
+  ) { }
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe(data => {
+      this.recipes = data;
+    });
+  }
+
+  getImageUrl(imageName: string | undefined): string {
+    if (!imageName) return this.defaultImage;
+    return `assets/${imageName}`;
+  }
+
+  showRecipeDetails(recipe: any): void {
+    this.selectedRecipe = recipe;
+    document.body.style.overflow = 'hidden';
+  }
+
+  closeRecipeDetails(): void {
+    this.selectedRecipe = null;
+    document.body.style.overflow = '';
+  }
+
+  deleteRecipe(id: number): void {
+    this.recipeService.deleteRecipe(id).subscribe(() => {
+      this.recipes = this.recipes.filter(recipe => recipe.id !== id);
+    });
+  }
+
+  editRecipe(id: string): void {
+    this.router.navigate(['/edit', id]);
+  }
+
+}
