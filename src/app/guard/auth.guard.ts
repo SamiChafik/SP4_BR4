9da1bb857e6b818699e9c5fd71a13c9b5560bc1c
SamiChafik <sami.chafik00@gmail.com>
2025-05-04 22:37:25 +0100

    auth guard to limit access

---
+++ src/app/guard/auth.guard.ts
@@ -0,0 +1,26 @@
+import { inject, Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  private service = inject(AuthService);
+  private router = inject(Router);
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+   
+    if (this.service.isLoggedIn()) {
+      return true;
+    }
+    else {
+      this.router.navigate(['login']);
+      return false;
+    }
+  }
+
+}
