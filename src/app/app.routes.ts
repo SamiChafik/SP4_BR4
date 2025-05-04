97328b0c1edbce011f846291b14cc827b057ccdd
SamiChafik <sami.chafik00@gmail.com>
2025-05-04 21:04:44 +0100

    routes to new components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecipeFormComponent } from './components/form/form.component';
+import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent},
     { path: 'add', component: RecipeFormComponent},
+    { path: 'recipes', component: RecipeListComponent},
     { path: '**', redirectTo: '' }
 ];
