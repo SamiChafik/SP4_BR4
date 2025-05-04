542d35660dd2499ffdf6e46fa2507f865452233f
SamiChafik <sami.chafik00@gmail.com>
2025-05-04 22:38:41 +0100

    added AuthGuard to some paths + new paths

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { RecipeFormComponent } from './components/form/form.component';
 import { RecipeListComponent } from './components/recipe-list/recipe-list.component';
+import { RegisterComponent } from './components/register/register.component';
+import { AuthGuard } from './guard/auth.guard';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent},
     { path: 'login', component: LoginComponent},
-    { path: 'add', component: RecipeFormComponent},
+    { path: 'add', component: RecipeFormComponent, canActivate:[AuthGuard]},
     { path: 'recipes', component: RecipeListComponent},
-    { path: 'edit/:id', component: RecipeFormComponent},
+    { path: 'edit/:id', component: RecipeFormComponent, canActivate:[AuthGuard]},
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
     { path: '**', redirectTo: '' }
 ];
