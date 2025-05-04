8a8b42e5d8cb2751ad8fe0d37b154cbd93fd9022
SamiChafik <sami.chafik00@gmail.com>
2025-05-04 23:10:43 +0100

    show last recipes in home

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,16 +1,39 @@
 import { Component, inject } from '@angular/core';
 import { Router } from '@angular/router';
+import { RecipeService } from '../../services/recipe.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
   private router = inject(Router);
 
+  recipes: any[] = [];
+  defaultImage = 'assets/logo_color.png';
+
+  constructor(private recipeService: RecipeService) { }
+
+  ngOnInit(): void {
+    this.recipeService.getRecipes().subscribe(data => {
+      this.recipes = data;
+    });
+  }
+
+  getImageUrl(imageName: string | undefined): string {
+    if (!imageName) return this.defaultImage;
+    return `assets/${imageName}`;
+  }
+
+  getLastFourRecipes(): any[] {
+    return [...this.recipes]
+      .sort((a, b) => b.id - a.id)
+      .slice(0, 4);
+  }
 
   GoToRecipes() {
     this.router.navigate(['recipes']);
